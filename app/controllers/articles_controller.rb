31b1bc02963ad100ba1f2635fe9168281071a864
jiayuzhou <dearjiayu@gmail.com>
2018-04-17 23:25:14 -0400

    add edit/update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,26 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		# Passing the article object to the method, 
+		# will automagically create url for submitting the edited article form. 
+		# This option tells Rails that we want this form to be submitted via the 
+		# PATCH HTTP method which is the HTTP method you're expected to 
+		# use to update resources according to the REST protocol.
+	    @article = Article.find(params[:id])
+	end
+
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
  
   		if @article.save  # validation successful. 
   			redirect_to @article	
+  			# there are lots of possible usages in redirect to. 
+  			# e.g., 
+  			#     redirect_to :controller => 'admins', :action => 'index'
+  			# for full details see here: https://apidock.com/rails/ActionController/Base/redirect_to
+
   		else  # when validation fails. 
   			render 'new'   # NOTE: no need to specifcy controller if in the same controller
 
@@ -27,6 +41,16 @@ class ArticlesController < ApplicationController
   			# otherwise, when the view access @article, it would give nil. 
   		end
   		
+  	end
+
+  	def update
+  		@article = Article.find(params[:id])
+
+  		if @article.update(article_params)
+  			redirect_to @article
+  		else
+  			render 'edit'
+  		end
   	end
 
   	private 
